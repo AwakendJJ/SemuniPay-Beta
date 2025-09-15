1092fc2c81806efdcb2438eb47e799dcba48cdc5
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-15 15:41:19 +0300

    feat: configure main.tsx to support wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+
+import { WagmiProvider, createConfig, http } from "wagmi"
+import { base, celo } from "wagmi/chains"
+import { metaMask } from "wagmi/connectors"
+
+const queryClient = new QueryClient()
+
+const config = createConfig({
+  chains: [base, celo],
+  connectors: [metaMask()],
+  transports: {
+    [base.id]: http(),
+    [celo.id]: http(),
+  },
+})
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>
-);
+)
