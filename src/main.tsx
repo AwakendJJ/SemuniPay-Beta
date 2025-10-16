397a86030bf66dcde623bc06cd0efd2eeedad958
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-10-16 21:13:41 +0300

    feat: customize conncetkit Button

--- src/main.tsx
+++ src/main.tsx
@@ -15,12 +15,19 @@ import { ConnectKitProvider } from "connectkit";
 
 
 const queryClient = new QueryClient()
-
+//   hover:bg-lime-300 transition-all duration-300
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>
+        <ConnectKitProvider customTheme={{
+    "--ck-connectbutton-background": "rgb(163 230 53)",
+    "--ck-connectbutton-color": "#111827",
+    "--ck-connectbutton-hover-background": "rgb(190 242 100)",
+    "--ck-connectbutton-active-background": "rgb(163 230 53)",
+    "--ck-connectbutton-border-radius": "9999px",
+    "--ck-connectbutton-font-size": "0.9rem"
+  }} >
         <App />
         </ConnectKitProvider>
       </QueryClientProvider>
