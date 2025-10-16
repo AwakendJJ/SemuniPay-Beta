899a18a249e7d093e1fcb32dc4a9ae5937fdbfde
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-10-16 20:51:48 +0300

    feat: add conncetkit provider

--- src/main.tsx
+++ src/main.tsx
@@ -7,26 +7,22 @@ import "./index.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 
-import { WagmiProvider, createConfig, http } from "wagmi"
-import { base, celo } from "wagmi/chains"
-import { metaMask } from "wagmi/connectors"
+import { WagmiProvider,  } from "wagmi"
+
+import { config } from './config'
+import { ConnectKitProvider } from "connectkit";
+
+
 
 const queryClient = new QueryClient()
-
-const config = createConfig({
-  chains: [base, celo],
-  connectors: [metaMask()],
-  transports: {
-    [base.id]: http(),
-    [celo.id]: http(),
-  },
-})
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>
         <App />
+        </ConnectKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   </StrictMode>
