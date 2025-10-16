503098607fc367eeb73f24807a509dc32ba2e07f
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-10-16 20:52:21 +0300

    feat: move wallet connection and wagmi to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi';
-import { config } from './config'
-import { ConnectKitProvider, getDefaultConfig } from "connectkit";
-
 
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
 import ConnectWallet from './pages/ConnectWallet';
 import VirtualCards from './pages/VirtualCards';
-const queryClient = new QueryClient()
 
 function App() {
   return (
-    <WagmiProvider config={config}>
-     <QueryClientProvider client={queryClient}>
-      <ConnectKitProvider>
+    
        <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -25,10 +17,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
-      </ConnectKitProvider>
-     </QueryClientProvider>
-     
-    </WagmiProvider>
+    
   );
 }
 
