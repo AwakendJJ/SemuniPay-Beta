c3272f4b2990b4574d3a30211c15466518019e72
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-10-27 23:50:15 +0300

    feat: dummy login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import {supabase} from '../supabaseClient'
+
+
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: ' http://localhost:5174/auth/callback' // redirect after clicking magic link
+      }
+    })
+    if (error) setMessage(error.message)
+    else setMessage('Magic link sent! Check your email.')
+  }
+
+  return (
+    <div>
+      <h2>Sign in with magic link</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={"text-gray-700"}
+        />
+        <button type="submit">Send Magic Link</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  )
+}
